83aca04f376cd63f44488fc477878cdb6f69c62e
Cleyton Fabio (pixies) <cleyton.flb@gmail.com>
2018-04-01 12:35:00 -0300

    add social_django.urls

--- simple_0auth/urls.py
+++ simple_0auth/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.contrib.auth import views
-from .views import home
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^login/$', views.login, name='login'),
-    url(r'^logout/$', views.logout, name='logout'),
-    url(r'^auth/', include('social_django.urls', namespace='social')),  # <- Here
-    url(r'^$', home, name='home'),
+    url(r'^account/', include('account.urls', namespace='account')),
+    #url(r'^account/', include('social_django.urls', namespace='social')),  # <- Here
+    url(r'^account/', include('social_django.urls', namespace='social')),  # <- Here
+#
 ]
